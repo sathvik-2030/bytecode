e9c06be354266e36e382c057d5f0ea081eea0ad3
kodam <kodam@Sathvik>
2024-07-01 17:11:44 +0530

    assisnment

---
+++ hello/src/hello/Rectangle.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Rectangle {
+	public static viod main (String[] args) {
+		byte lenght = 5;
+		byte widht = 7;
+		int area_of_the_rectangle = lenght*widht;
+		int parimeter = 2*(lenght+widht);
+		
+		System.out.println(area_of_the_rectangle);
+		System.out.println(parimeter);
+	}
+
+}
